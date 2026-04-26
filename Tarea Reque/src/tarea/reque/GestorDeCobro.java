ff142ee0eeea87d22444429973001981938de184
Alli2607 <allisonuniversidad0@gmail.com>
2026-04-26 11:55:21 -0600

    pequeño avance

--- Tarea Reque/src/tarea/reque/GestorDeCobro.java
+++ Tarea Reque/src/tarea/reque/GestorDeCobro.java
@@ -1,8 +1,18 @@
 
 package tarea.reque;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class GestorDeCobro {
+    private List<Transaccion> listaTransacciones;
+    private DetalleDeCobro detalleDeCobro;
+
+    public GestorDeCobro() {
+        this.listaTransacciones = new ArrayList<>();
+        this.detalleDeCobro = new DetalleDeCobro();
+    }
     
     public int calcularMonto(){
         return 0;
