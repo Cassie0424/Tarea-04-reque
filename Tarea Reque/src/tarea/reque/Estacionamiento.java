52fb9aeeb935a968382cc6859598d31ebcb9cace
ferabarca1910 <106549297+ferabarca1910@users.noreply.github.com>
2026-04-26 23:00:34 -0600

    Update Estacionamiento.java
    Estacionamiento,modificado para diagrma de h.u.2

--- Tarea Reque/src/tarea/reque/Estacionamiento.java
+++ Tarea Reque/src/tarea/reque/Estacionamiento.java
@@ -1,14 +1,34 @@
-
 package tarea.reque;
 
-
 public class Estacionamiento {
-    
-    public boolean verificarDisponibilidad(){
+
+    private String idEstacionamiento;
+    private String idUsuario;
+    private int tiempoRestanteMinutos;
+
+    public Estacionamiento() {
+        this.idEstacionamiento = "EST-001";
+        this.idUsuario = "estudian@tec.com";
+        this.tiempoRestanteMinutos = 5;
+    }
+
+    public boolean verificarDisponibilidad() {
         return true;
     }
-    
-    public void registrarHora(String tipo){}
-    
-    public void consultarEstacionamiento(){}
+
+    public void registrarHora(String tipo) {}
+
+    public void consultarEstacionamiento() {}
+
+    // Tiempo_Restante
+    public int obtenerTiempoRestante(String idEstacionamiento) {
+        System.out.println("Consultando tiempo restante para: " + idEstacionamiento);
+        return tiempoRestanteMinutos;
+    }
+
+    // Solicita_Usuario
+    public String solicitarDatosUsuario(String idEstacionamiento) {
+        System.out.println("Obteniendo datos del usuario para: " + idEstacionamiento);
+        return idUsuario;
+    }
 }
